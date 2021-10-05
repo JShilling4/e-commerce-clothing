5f073f5cc4dcc77da94a7c014ce48e1c959fcab4
Justin Shilling <loveofmusic25@hotmail.com>
2021-10-04 22:28:54 -0400

    google sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+    apiKey: "AIzaSyC1VXOcpRYOjXFqNyR7j4MwVC8xC-iGDOM",
+    authDomain: "e-commerce-clothing-96091.firebaseapp.com",
+    projectId: "e-commerce-clothing-96091",
+    storageBucket: "e-commerce-clothing-96091.appspot.com",
+    messagingSenderId: "132712519177",
+    appId: "1:132712519177:web:2cc144aa697eced0a04ad8",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
