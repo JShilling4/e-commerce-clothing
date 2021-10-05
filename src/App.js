2b63b083b8edf8258bf3da9818204515a0e88832
Justin Shilling <loveofmusic25@hotmail.com>
2021-10-04 23:06:25 -0400

    user persist

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./components/shop/shop.component";
 import SignInPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
     constructor() {
@@ -18,9 +18,26 @@ class App extends React.Component {
         };
     }
 
+    unsubscribeFromAuth = null;
+
     componentDidMount() {
-        auth.onAuthStateChanged((user) => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot((snapshot) => {
+                    this.setState({
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data(),
+                        },
+                    }, () => {
+                        console.log(this.state);
+                    });
+                });
+            } else {
+                this.setState({ currentUser: userAuth });
+            }
         });
     }
 
